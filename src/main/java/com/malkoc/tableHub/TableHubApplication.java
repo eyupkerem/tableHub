8e38f62b070736eb67121101b253b4786fc9c3f4
eyupkerem <malkockerem1312@gmail.com>
2024-12-11 17:11:18 +0300

    initial commit

---
+++ src/main/java/com/malkoc/tableHub/TableHubApplication.java
@@ -0,0 +1,13 @@
+package com.malkoc.tableHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TableHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/malkoc/tableHub/TableHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malkoc.tableHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
